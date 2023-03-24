58b0b5fcde346a907af7be4ba846a71cbb18e5ef
Niray Veli <63594484+NirayVeli@users.noreply.github.com>
2023-03-24 12:41:09 +0200

    Add files via upload

---
+++ ASCII stuff.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+		//finding ASCII code of a symbol:
+	cout << (int)'A' << endl;
+
+		//finding symbol with an ASCII code:
+	cout << (char)69 << endl;
+
+		// true or false:
+	cout << ('B' < 'D') << endl << ('A' == 'a') << endl;
+
+return 0;
+}
