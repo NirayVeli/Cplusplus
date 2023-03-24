266d49f4e90fd30bf97dd3dd5969c45ed37af5aa
Niray Veli <63594484+NirayVeli@users.noreply.github.com>
2023-03-24 12:48:43 +0200

    Add files via upload

---
+++ Math numbers.cpp
@@ -0,0 +1,21 @@
+// example: 532 -> 5+3+2 = 10
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+	int number;
+	cout << "Number: ";
+	cin >> number;
+	while (number < 0 || number>1000) {
+		cout << "Enter number between 1-999!" << endl;
+		cin >> number;
+	}
+		int n1 = number % 10;
+		int n2 = number / 10 % 10;
+		int n3 = number / 100;
+		cout << "Total: " << n1 + n2 + n3 << endl;
+
+	return 0;
+}
